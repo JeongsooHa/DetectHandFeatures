b86b6e05542ed7d2dab4a5aba8a5c918bc5510ac
하정수 <jeongsooha@hajeongsoo-MacBook-Pro.local>
2017-08-12 15:12:00 +0900

    Initial Commit

---
+++ HandDetectUsingCircle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HandDetectUsingCircle
+//
+//  Created by 하정수 on 12/08/2017.
+//  Copyright © 2017 하정수. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
